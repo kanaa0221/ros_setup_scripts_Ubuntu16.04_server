676ccc95a36f02da72ae994da40a00c0aa1f8f83
kanaa0221 <51359517+kanaa0221@users.noreply.github.com>
2019-06-05 15:53:32 +0900

    Update step1.bash

--- step1.bash
+++ step1.bash
@@ -8,7 +8,7 @@ echo "deb http://packages.ros.org/ros/ubuntu $UBUNTU_VER main" > /tmp/$$-deb
 sudo mv /tmp/$$-deb /etc/apt/sources.list.d/ros-latest.list
 
 sudo apt-get install -y curl
-curl -k https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
+#curl -k https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
 sudo apt-get update || echo ""
 
 sudo apt-get install -y ros-${ROS_VER}-ros-base
